dd2d9839115cfd2c9a052ae2dac04881048ce544
hectorbuono <hectorbuono@gmail.com>
2026-04-28 02:05:27 -0300

    Correccion para desplegar

--- App-Vite/ClashRoyale/vite.config.js
+++ App-Vite/ClashRoyale/vite.config.js
@@ -1,26 +1,7 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Buscamos la Key en el archivo .env
-  const env = loadEnv(mode, '.', '')
+export default defineConfig ({
 
-  return {
     plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'https://royaleapi.dev',
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-          headers: {
-            // Vite pega la Key aquí, el navegador no la ve
-            'Authorization': `Bearer ${env.VITE_CLASH_ROYALE_API_KEY}`,
-            'Accept': 'application/json'
-          }
-        }
-      }
-    }
-  }
-})
+    base:"App-Vite/ClashRoyale"})
